056f95cb009071798a0dda40cd36ef983386701d
Happy Coding UK <57214237+happycodinguk@users.noreply.github.com>
2022-04-12 19:05:45 +0100

    Update csf-lfd-notifications.sh

--- csf-lfd-notifications.sh
+++ csf-lfd-notifications.sh
@@ -2,26 +2,22 @@
 ######################################################################
 ######## CSF LFD Notifications #######################################
 ######################################################################
-# Hope people enjoy the many hours I have put into this project!
-# This project is created with my knowledge and some snippets from 
-# across the open web. If there are snippets you would like credit for. 
-# Drop me a line and I'll add you!
-# Anyone. Feel free to clone and improve.
-####################################################
-# DM me on:                                        #
-# Telegram: https://t.me/joinchat/xlmtm7jVYR4yODQ0 #
-####################################################
-
+# You will need to enable the relevant notifications in your CSF config file
+# ie LF_SSH_EMAIL_ALERT etc. If these aren't enabled the log file won't have 
+# log entries to send via telegram
+######################################################################
 
 source telegram.conf
 echo "$token" > /dev/null 2&>1
 echo "$chat_id" > /dev/null 2&>1
 
-#These are emoji codes 
+#Below are emoji codes ####################################### 
 # go to https://unicode.org/emoji/charts/full-emoji-list.html
 # you will need its "U+1F4EB" code MINUS the + As seen below
+##############################################################
 padlock=$'\U1F512'
 lookingglass=$'\U1F50D'
+
 #Telegram API to send notification.
 function telegram_send
 {
@@ -36,17 +32,13 @@ tail -fn0 /var/log/lfd.log |
 while read -r line
 do
    [[ "$line" != *"*"* ]] && continue
-   echo $line > /tmp/lfd.txt
+   echo $line > /tmp/lfd.txt 
 
 
 message=$(</tmp/lfd.txt)
-echo "$message"
+echo "$message" > /dev/null 2&>1
 
-telegram_send
-
+telegram_send > /dev/null 2&>1
 rm -r /tmp/lfd.txt
-sleep 5
 done
-
-
 
